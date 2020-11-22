62656623f301d8049a7bbe21a1659cc18a143553
Ellie Brayton <ebrayton@ufl.edu>
2020-11-21 23:10:50 -0600

    Finished generator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # m12-hw12-brayton-ellie
+
+Structuring event listeners with arrow functions
+https://medium.com/@josephcardillo/arrow-functions-and-this-in-es6-4f1d350a85cf
+
+W3Schools
+https://www.w3schools.com/jsref/prop_img_src.asp
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const myButton = document.getElementById('randomizer');
 myButton.addEventListener('click', () => {
     
 
-const doggos = ['Bread Boi!', 'Cloud Boi!', 'Smol Bean!'];
+const doggos = ['Bread Boi', 'Cloud Boi', 'Smol Bean', 'Heckin Concerned Doggo', 'Thicc Boi', 'Pupperina', 'Long Boi', 'Shibe', 'Woofer','Wrinkly Boi'];
 
 let shuffle = (i, j, k) => {
     for (i = doggos.length - 1; i > 0; i--) {
@@ -15,16 +15,40 @@ let shuffle = (i, j, k) => {
 
 
 
-if (doggos[0] == 'Bread Boi!') {
+if (doggos[0] == 'Bread Boi') {
     document.getElementById('image').src = 'img/bread-boi.jpg';
 }   
 
-else if (doggos[0] == 'Cloud Boi!') {
+else if (doggos[0] == 'Cloud Boi') {
     document.getElementById('image').src = 'img/cloud-boi.jpg';
 }   
-else if (doggos[0] == 'Smol Bean!') {
+else if (doggos[0] == 'Smol Bean') {
     document.getElementById('image').src = 'img/smol-bean.jpg';
-}                                            
+} 
+else if (doggos[0] == 'Long Boi') {
+    document.getElementById('image').src = 'img/long-boi.jpg';
+}         
+else if (doggos[0] == 'Heckin Concerned Doggo') {
+    document.getElementById('image').src = 'img/heckin-concerned.jpg';
+}                       
+else if (doggos[0] == 'Thicc Boi') {
+    document.getElementById('image').src = 'img/thicc-boi.jpg';
+}        
+else if (doggos[0] == 'Pupperina') {
+    document.getElementById('image').src = 'img/pupperina.jpg';
+}      
+else if (doggos[0] == 'Long Boi') {
+    document.getElementById('image').src = 'img/long-boi.jpg';
+}      
+else if (doggos[0] == 'Shibe') {
+    document.getElementById('image').src = 'img/shibe.jpg';
+}   
+else if (doggos[0] == 'Woofer') {
+    document.getElementById('image').src = 'img/woofer.jpg';
+}  
+else if (doggos[0] == 'Wrinkly Boi') {
+    document.getElementById('image').src = 'img/wrinkly-boi.jpg';
+}  
 };
 }; 
 
